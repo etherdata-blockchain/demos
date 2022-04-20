9829f0b9f1f6e50910cfc2f5d288c664e388528b
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-04-20 14:18:56 +0800

    feat: add ui packages (#3)

--- examples/file-upload/next.config.js
+++ examples/file-upload/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTM = require("next-transpile-modules")(["ui"]);
 
-module.exports = nextConfig
+module.exports = withTM({
+  reactStrictMode: true,
+});

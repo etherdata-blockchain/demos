8863855145c0aa49f37b6c5ba1d092f3a13399cb
ZHU Xinyi <1063185542@qq.com>
2022-04-27 00:58:06 +0800

    Change file name.

--- examples/transaction/next.config.js
+++ examples/transaction/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["ui", "file-storage-contract"]);
+const withTM = require("next-transpile-modules")(["ui", "file-storage-contract", "votingdapp"]);
 
 module.exports = withTM({
   reactStrictMode: true,
